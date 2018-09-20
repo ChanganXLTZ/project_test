b288ab9c163d58ea28ce40f776d602d0000d2e44
ChanganXLTZ <43316823+ChanganXLTZ@users.noreply.github.com>
2018-09-20 09:05:07 +0800

    Delete ex04_print.py

--- ex04_print.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys;x = '1run';sys.stdout.write(x + '\n'+x)
-# 不同语句用分号隔开可以同行
-
-print("a")
-print("b") #默认换行输出
-print("a",end="") #不换行输出
-print("b",end=" ")
-print("c",end=" ")
